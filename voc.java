3a7e03e0b01669bbf7c351289e93929fa00adcba
gokulgokul <gokulgokul@users.noreply.github.com>
2016-08-20 17:09:48 +0530

    Add files via upload

---
+++ voc.java
@@ -0,0 +1,17 @@
+
+import java.io.IOException;
+
+public class Vow {
+
+    public static void main(String[] args) throws IOException
+    {
+      char c;
+      c=(char)System.in.read();
+      if(c=='a'||c=='e'||c=='i'||c=='o'||c==u)
+          System.out.println("vowels");
+      else
+          System.out.println("consonant");
+          
+    }
+    
+}
